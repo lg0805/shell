eb1434649cc0911148e8b1edd656ad96e3c962be
Li Guang <30360993+lg0805@users.noreply.github.com>
2023-04-17 15:15:31 +0800

    Update expect_scp.sh

--- expect_scp.sh
+++ expect_scp.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/expect -f
-# 获取第一个位置参数
+
+# This script sets variables for IP address, 
+# username, password, and timeout used in login. 
+# The IP address is set to the first argument in the script's argument list, 
+# while the username is set to "root" and the password is set to "p@ssw0rd". 
+# The timeout is set to 5 seconds.
 set ip [lindex $argv 0]
 set user root
 set password p@ssw0rd
 set timeout 5
 
 spawn scp -r /etc/hosts $user@$ip:/tmp
+
+# 这段代码用于自动化需要进行yes/no响应和密码输入的过程。
+# 当它接收到"yes/no"时，它会发送字符串"yes\r"，
+# 而当它接收到"password："字符串时，它会发送变量"$password\r"。
+# exp_continue命令告诉程序在发送响应后继续查找更多输入。
 expect{
     "yes/no" {
         send "yes\r"
@@ -16,3 +26,4 @@ expect{
     }
 }
 expect eof
+
